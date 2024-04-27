eec85c46cc0fe9c3fb6871b3f29d22af1c6ab6a3
Mohsen Ameer <mohsenameer15@gmail.com>
2024-04-27 10:43:01 +0300

    updating

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Cards from "./components/Cards";
+import Food from "./components/Food";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Category from "./components/Category";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Hero />
+      <Cards />
+      <Food />
+      <Category />
     </div>
   );
 }
